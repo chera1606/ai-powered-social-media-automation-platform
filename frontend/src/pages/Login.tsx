d01c931e1722d852c3225a4fefbb356f0f8642f1
Cherinet Derbie <cherinetderbie@gmail.com>
2026-09-11 23:19:27 +0300

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { MailIcon, LockIcon, ArrowRightIcon, User2Icon } from "lucide-react";
-
 export default function Login() {
     const [loginState, setLoginState] = useState(true);
     const [name, setName] = useState("");
